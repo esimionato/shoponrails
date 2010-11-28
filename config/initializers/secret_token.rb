631a7f657e205cea78f2e851eaf2101529346d75
Magdalena Sikorska <madzia.sikorska@gmail.com>
2010-11-28 17:33:00 +0100

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shoponrails::Application.config.secret_token = '3757c43438b754de11471d05af8b7c7b47db30f1d3808dcb135a3dba9139ecc2601dfda9edbfd81aefb33ce0b192e148e3494a545d4f2ad19c4d04f50d37086a'
